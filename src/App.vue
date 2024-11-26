df5410418b39e900995642400ab01813174a1d37
Hash2O <archeosam@gmail.com>
2024-11-26 22:10:17 +0100

    Quiz & ProgressBar Components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
-  <div>
-    {{ quiz }}
+  <div class="container">
+    <div v-if="state === 'error'">
+    <p>
+      Impossible de charger le quiz.
+    </p>
+  </div>
+  <div :aria-busy="state === 'loading'">
+    <Quiz :quiz="quiz" v-if="quiz" />
+  </div>
   </div>
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue';
+import Quiz from './components/Quiz.vue';
 
 const quiz = ref(null)
+const state = ref('loading')
 
 onMounted(() => {
   fetch('/quiz.json')
@@ -19,6 +28,16 @@ onMounted(() => {
     })
     .then(data => {
       quiz.value = data
+      state.value = 'idle'
+    })
+    .catch(e => {
+      state.value = 'error'
     })
 })
 </script>
+
+<style>
+.container {
+  margin-top: 2rem;
+}
+</style>
---
+++ src/components/Quiz.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>{{ quiz.title }}</h1>
+    <ProgressBar :value="step" :max="quiz.questions.length - 1" />
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import ProgressBar from './ProgressBar.vue';
+
+    defineProps({
+        quiz: Object
+    })
+
+    const step = ref(0)
+</script>
