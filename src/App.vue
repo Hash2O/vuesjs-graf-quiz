9479052c4e0dd21b2b00234f603f89c88d557cdb
Hash2O <archeosam@gmail.com>
2024-11-26 21:29:04 +0100

    Setup, App cleaning, Json file

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    {{ quiz }}
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const quiz = ref(null)
+
+onMounted(() => {
+  fetch('/quiz.json')
+    .then(result => {
+      if(result.ok) {
+        return result.json()
+      }
+      throw new Error("Impossible de récupérer le fichier json")
+    })
+    .then(data => {
+      quiz.value = data
+    })
+})
+</script>
